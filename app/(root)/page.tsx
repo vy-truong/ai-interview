8f98b522f33f0c1e5df6d0779f87b365e7417052
Vy Truong <144472965+vy-truong@users.noreply.github.com>
2025-11-05 03:27:18 -0700

    Working Updated Version: global.css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview Ready with AI-Powered practice & Feedback</h2>
+          <p className='text-lg'>
+            Practice on real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href="/interview">
+              Start an Interview
+            </Link>
+          </Button>
+        </div>
+
+        <Image src="/robot.png" alt="robot-dude" width={400} height={400}/>
+      </section>
+    </>
   )
 }
 
