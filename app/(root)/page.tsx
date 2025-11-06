b9f2f3606eb4b5cf390796cb622bba9a1ae80bfb
Vy Truong <144472965+vy-truong@users.noreply.github.com>
2025-11-05 20:12:55 -0700

    Create interview card components.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,18 +29,18 @@ const Page = () => {
       <section className='flex flex-col gap-6 mt-8'>
           <h2>Your Interview</h2>
 
-          <div className='interview-section'>
+          <div className='interviews-section'>
             {dummyInterviews.map((interview) => (
               <InterviewCard {...interview} key={interview.id}/>
             ))}
 
-            <p>You haven&apos;t taken any interviews yet</p>
+            {/* <p>You haven&apos;t taken any interviews yet</p> */}
           </div>
       </section>
 
       <section className='flex flex-col gap-6 mt-8'>
           <h2>Take an Interview</h2>
-          <div className='interview-section'>
+          <div className='interviews-section'>
             {dummyInterviews.map((interview) => (
               <InterviewCard {...interview} key={interview.id}/>
             ))}
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import dayjs from 'dayjs';
 import Image from 'next/image';
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+import { getRandomInterviewCover } from '@/lib/utils';
+import { Button } from './ui/button';
+import Link from 'next/link';
+const InterviewCard = async ({
+    interviewId,
+    userId,
+    role,
+    type,
+    techstack,
+    createdAt,
+  }: InterviewCardProps) => { 
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY'); 
@@ -11,7 +21,8 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: 
   return (
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
         <div className='card-interview'>
-            <div>
+         
+                {/* badge tech or behav */}
                 <div className='absolute top-0 right-0 w-fit  px-4 py-2 rounded-bl-lg bg-light-600'>
                     <p className='badge-text'>{normalizedType}</p>
                 </div>
@@ -20,9 +31,43 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: 
                 alt='cover image'
                 width={90} 
                 height={90}
-                className='rounded-full object-fit size-[90px]'
+                className='rounded-full object-fit size-[70px]'
                 />
-            </div>
+
+                <h3 className='mt-2 capitalize'>
+                    {role} Interview
+                </h3>
+
+                <div className='flex flex-row gap-5 mt-3'>
+                    <div className='flex flex-row gap-2'>
+                        <Image src="/calendar.svg" alt="calendar" width={22} height={22} />
+                        <p>{formattedDate}</p>
+                    </div>
+                </div>
+                <div className='flex flex-row gap-2 items-center'>
+                            <Image src="/star.svg" alt="star" width={22} height={22} className=''/>
+                            <p>{feedback?.totalScore || '---'}/100</p>
+                </div>
+
+                <p className='line-clamp-2 mt-3'>
+                    {feedback?.finalAssessment || "You havent taken the interview yet. Take it now to improve your skills."}
+                </p>
+
+                <div className='flex flex-row justify-between'>
+                    <p>Tech Icon</p>
+
+                    <Button className='btn-primary'>
+                        <Link href={feedback
+
+                            ? `/interview/${interviewId}/feedback`
+                            : `/interview/${interviewId} `
+                        }>  
+                            {feedback ? 'Check feedback' : 'View Interview'}
+                        </Link>
+                    </Button>
+
+                </div>
+        
         </div>
     </div>
   )
